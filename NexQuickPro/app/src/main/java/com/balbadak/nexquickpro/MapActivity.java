94ad9acd28bea21c9b8c027e2c9978c3f8b51c88
TaejinHWang <tomguy@hanmail.net>
2018-06-08 10:01:23 +0900

    2018-06-08 10:00 Pro앱 Fragment2_2(기사 위치 계속 수정하면서 서버로 보내는거 진행중)

---
+++ NexQuickPro/app/src/main/java/com/balbadak/nexquickpro/MapActivity.java
@@ -0,0 +1,23 @@
+package com.balbadak.nexquickpro;
+
+import android.app.Activity;
+import android.location.Location;
+
+import com.skt.Tmap.TMapGpsManager;
+
+public class MapActivity extends Activity implements TMapGpsManager.onLocationChangedCallback {
+
+    public MapActivity(){
+        TMapGpsManager gps = new TMapGpsManager(this);
+        gps.setProvider(gps.GPS_PROVIDER);
+        gps.setMinTime(1000);
+        gps.setMinDistance(5);
+        gps.OpenGps();
+    }
+
+
+    @Override
+    public void onLocationChange(Location location) {
+
+    }
+}
